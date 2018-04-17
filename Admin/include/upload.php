d1d689251da4d5529d8772fb7ec742537426a8e3
Sneha <sneha@royalsoftwaresolution.com>
2018-04-17 09:54:15 +0530

    today

--- Admin/include/upload.php
+++ Admin/include/upload.php
@@ -6,25 +6,36 @@ $name = $_POST["name"];
 $category = $_POST["category"];
 $uploaddate = date("Y-m-d");
 
+
 if (!is_dir($email)) {
     mkdir($email);         
 }
 
-for($i=0; $i<count($_FILES['file']['name']); $i++){
-	$ext = explode('.', basename( $_FILES['file']['name'][$i]));
+   // upload file here if the extension matches to your allowed ones.
 
+for($i=0; $i<count($_FILES['file']['name']); $i++){
+	
+  
+   
+
+	$ext = explode('.', basename( $_FILES['file']['name'][$i]));
+   
 	$target_path = $email."/".$ext[0]."_".date("h_m_s").".".$ext[1]; 
 	if(move_uploaded_file($_FILES['file']['tmp_name'][$i], $target_path)) {
 	    $query = "INSERT INTO `rsm_files` (`name`, `category`, `filename`, `path`, `email`, `uploaddate`) VALUES ('".$name."' ,'".$category."' ,'".$ext[0]."' ,'". $target_path."' ,'". $email."','". $uploaddate."')";
 	    $res = query($query);
+
 		if($res){		
 			echo json_encode(array('Status' => "1" ));
 			}else{
 				echo json_encode(array('Status' => mysqli_error($conn)));
 			}
 		}
+
 	 else{
 	    echo json_encode(array("status"=>0));
 		}
 }
+
+
 ?>

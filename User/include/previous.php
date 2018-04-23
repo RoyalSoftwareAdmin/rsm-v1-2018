72259c686f4c5c0670a4fc46b8d9bd8845e6da4e
Sneha <sneha@royalsoftwaresolution.com>
2018-04-23 08:23:17 +0530

    today

--- User/include/previous.php
+++ User/include/previous.php
@@ -4,36 +4,8 @@
     <div class="col-md-12 content-top-2 card">
         <div class="agileinfo-cdr">
             <div class="card-header">
-                
-                <div class="container-fluid">
-                <div class="row">
-                	<div class = "col-md-6">
-                		<label>Enter New Password</label>
-                		<input type="password" name="password" id="password" placeholder="Enter New Password" >
-                        
-                	</div>
-                	
-                	<div class = "col-md-6">
-                		<label>Enter Confirm Password</label>
-                		<input type="password" name="repassword" id="repassword" placeholder="Enter Confirm Password" >
-                	</div>
-
+                <h3> your previous test data here </h3>
                 </div>
-
-                
-                	<div class="container-fluid">
-                		<div class="sub_home">
-                			<div class="row">
-                				<div class="col-md-12">
-								<input type="button" id="submit" value="Submit">
-							
-						</div>
-					</div>
-                       <div class="error"></div>
-                </div>
-                </div>
-            </div>
-            </div>
         </div>
         </div>
     </div>      

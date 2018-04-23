f3e27106a6ace38282e707e4062b14b0b4038139
Sneha <sneha@royalsoftwaresolution.com>
2018-04-23 08:47:20 +0530

    today

--- Admin/js/modules/uploadDocs.js
+++ Admin/js/modules/uploadDocs.js
@@ -51,3 +51,14 @@ $(document).ready(function(){
         
     });
 })
+
+
+    document.getElementById('files').addEventListener('change', function(e) {
+  var list = document.getElementById('filelist');
+  list.innerHTML = '';
+  for (var i = 0; i < this.files.length; i++) {
+    list.innerHTML += (i + 1) + '. ' + this.files[i].name + '\n';
+  }
+  if (list.innerHTML == '') list.style.display = 'none';
+  else list.style.display = 'block';
+});

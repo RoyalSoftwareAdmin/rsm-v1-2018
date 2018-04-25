777d27df98a046467143d0ac380972cc9c1d4816
Sneha <sneha@royalsoftwaresolution.com>
2018-04-25 08:28:29 +0530

    today changrs on uploadDocs.php

--- Admin/include/uploadDocs.php
+++ Admin/include/uploadDocs.php
@@ -22,6 +22,9 @@
 							    
 					<input type="hidden" name="email" id="email">
 					<input type="hidden" name="name" id="name">
+					
+					
+					
 					   
 					 <div class="col-md-4">
             		<div class="sign-u">
@@ -40,6 +43,16 @@
 				        </div>
 				    </div>
 
+				    <!--start-->
+
+				    <div class="row">
+				    	<div class="col-md-12">
+				    		<p >Amount</p>
+			      	<input id="amount" type="text" name="amount">
+				    		</div>
+				    </div>
+				    <!--end-->
+
  </div>
 
 					      
@@ -57,6 +70,19 @@
     </div>      
     <div class="clearfix"> </div>
 </div>
+<!--<script type="text/javascript">
+$(document).ready(function(){
+	  
+function testInput(event) {
+   var value = String.fromCharCode(event.which);
+   var pattern = new RegExp(/[0-9]/i);
+   return pattern.test(value);
+}
+$('#amount').bind('keypress', testInput);
+
+
+  })
+</script>-->
 
 	
 <script src="js/modules/uploadDocs.js"></script>  

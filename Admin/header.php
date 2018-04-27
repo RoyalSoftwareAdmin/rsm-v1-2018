c9666ff8dc4c63aa41651dfced12bb450a94413d
Sneha <sneha@royalsoftwaresolution.com>
2018-04-27 22:16:20 +0530

    today changes on Admin/header.php

--- Admin/header.php
+++ Admin/header.php
@@ -204,10 +204,10 @@ session_start();
 				
 				
 				<!--search-box-->
-				<div class="search-box">
+				<div class="search-box" >
 					<form class="input">
 						<input class="sb-search-input input__field--madoka" placeholder="Search..." type="search" id="input-31" />
-						<label class="input__label" for="input-31">
+						<label class="input__label" for="input-31" >
 							<svg class="graphic" width="100%" height="100%" viewBox="0 0 404 77" preserveAspectRatio="none">
 								<path d="m0,0l404,0l0,77l-404,0l0,-77z"/>
 							</svg>
@@ -220,7 +220,7 @@ session_start();
 						<li class="dropdown profile_details_drop">
 							<a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
 								<div class="profile_img">	
-									<span class="prfil-img"><img src="../../images/2.jpg" alt=""> </span> 
+									<span class="prfil-img"><img src="../images/.jpg" alt=""> </span> 
 									<div class="user-name">
 										<p>User</p>
 										<span class="loggeduser"></span>

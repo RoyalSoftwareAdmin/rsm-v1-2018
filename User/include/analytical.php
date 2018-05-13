245e268d60019e1bb5db2e6dafc989b624c27424
Sneha <sneha@royalsoftwaresolution.com>
2018-05-13 17:14:05 +0530

    today changes on User/include/analytical.php

--- User/include/analytical.php
+++ User/include/analytical.php
@@ -7,30 +7,30 @@
                 <!--<h3> Analytical Skills are basic need for any startup. When a project starts, one should analyze the probablity of acheiving it in all possible solution and bring the best of it.</h3><br><br>-->
             </div>
 			
-		<div class="container">
+		<div class="container divParent">
 		 <div class="row">
 		   
 		   <div class="col-md-4">
-		     <a href=""><button class="comm">Communication</button></a>
+		     <a href="userinstructions.php"><button class="comm" data-val="10">Communication</button></a>
 		   </div>
 		   
 		   <div class="col-md-4">
-		    <a href=""><button class="creativity">Creativity</button></a>
+		    <a href="userinstructions.php"><button class="creativity" data-val="11">Creativity</button></a>
 		   </div>
 		   
 		   <div class="col-md-4">
-		    <a href=""><button class="critical">Critical Thinking</button></a>
+		    <a href="userinstructions.php"><button class="critical" data-val="12">Critical Thinking</button></a>
 		   </div>
 		   </div>
 		   
 		  <div class="row"> 
 		   
 		   <div class="col-md-4">
-		   <a href=""> <button class="dataanalysis">Data Analysis</button></a>
+		   <a href="userinstructions.php"> <button class="dataanalysis" data-val="13">Data Analysis</button></a>
 		   </div>
 		   
 		    <div class="col-md-4">
-		    <a href=""><button class="research">Research</button></a>
+		    <a href="userinstructions.php"><button class="research" data-val="14">Research</button></a>
 		   </div>
 		   
 		 </div>
@@ -48,4 +48,5 @@
 		
  </div>      
     <div class="clearfix"> </div>
+   
 <script src="js/modules/analytical.js"></script> 

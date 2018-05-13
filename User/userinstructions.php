7137805d5f301469c2637f72c719843ce98f502f
Sneha <sneha@royalsoftwaresolution.com>
2018-05-13 17:07:46 +0530

    today changes on User/userinstructions.php

--- User/userinstructions.php
+++ User/userinstructions.php
@@ -17,105 +17,85 @@
 
 	
 	 
-	 <!-- js-->
-	<script src="../js/jquery-1.11.1.min.js"></script>
-	<script src="../js/modernizr.custom.js"></script>	
+	
+	
 	<!--webfonts-->
 	<link href="//fonts.googleapis.com/css?family=PT+Sans:400,400i,700,700i&amp;subset=cyrillic,cyrillic-ext,latin-ext" rel="stylesheet">
 	<!--//webfonts--> 
 
-	<!-- chart -->
-	<script src="../js/Chart.js"></script>
-	<!-- //chart -->
-
-	<!-- Metis Menu -->
+	
 	
 	
 	<link rel="stylesheet" href="../css/export.css" type="text/css" media="all" />
 	<link rel="stylesheet" href="css/modules/userinstructions.css">
-	<script type="text/javascript" src="js/common/common.js"></script>
-	<script src="../js/pie-chart.js" type="text/javascript"></script>
-	<script src="../js/owl.carousel.js"></script>
-	<script src="../js/amcharts.js"></script>
-	<script src="../js/serial.js"></script>
-	<script src="../js/export.min.js"></script>
-	<script src="../js/light.js"></script>
-	<script src="../js/jquery.dataTables.js"></script>
-	<script src="../js/dataTables.bootstrap.js"></script>
 	
-</head>
-<body>
-  <div class="container">
-    <div class="row">
-      <div class="col-md-4">
-        <img src="../images/logo.png">
-       </div>
-       <div class="col-md-4">
-       <h1>Instructions</h1>
-       </div>
-   </div>
+	
 
-       <div class="row" >
-       	<div class="col-md-8 col-md-offset-1">
-       		<label>General Instructions</label>
-       		<ul>
-       			<li> <span><img src="../images/right.png" class="im"></span>
-       			<p class="q">Question is submitted</p></span></li>
+
+</head>
+<body class="cbp-spmenu-push">
+<div class="main-content">
+           <div class="logo1">
+			<img src= "../images/logo.png"/>
+		</div>
+			<!-- main content start-->
+			<div id="page-wrapper">
+		
+				<div class="main-page login-page ">
+     
+					
+					<div class="widget-shadow">
+            
+						<div class="login-body">
+             
+               <h1 align="center">Instructions</h1>
+            
+					   <h3>General Instructions</h3>
+          <ul>
+            <li> <span><img src="../images/right.png" class="im"></span>
+            <p class="q">Question is submitted</p></span></li>
               <li> <span><img src="../images/eyes.png" class="ima"></span>
             <p class="q">Question is kept for review</p></span></li>
-       			<li> <span><img src="../images/wrong.jpg" class="im"></span>
-       			<p class="q">Unanswered questions</p></li>
-       		<li> 
-       			<p class="q">Keep eye on countdown timer(look up at right top corner reverse clock)</p></li>
-       			<li> 
-       			<p class="q">Dont forget to click on save and exit button</p></li>
-       			<li> 
-       			<p class="q">Reviews answers will not be saved unless Save and Exit button is clicked</p></li>
-       		</ul>
-       	</div>
-       </div>
-       <div class="row">
-       		<div class="col-md-8 col-md-offset-1">
-       		<h3>Dos,</h3>
-       		<ul>
-       			<li><p>Fill all mandatory fields given in registration form without any errors</p></li>
-       				<li><p>You can choose any random question in the list</p></li>
-       					<li><p>You can keep review for the answer which is not sure</p></li>
-       						<li><p>You can carry only pen and paper to the test hall</p></li>
-       							<li><p>Any doubts inbetween online test,you can ask invigilator</p></li>
-       		</ul>
-       </div>
-       </div>
-        <div class="row">
-       		<div class="col-md-8 col-md-offset-1">
-       		<h3>Dont's</h3>
-       		<ul>
-       			<li><p>Should not carry any electonics gadgets</p></li>
-       				<li><p>Don't use any functional keys</p></li>
-       					<li><p>Do not resize the test window</p></li>
-       						<li><p>Do not refresh the page when test is conducting</p></li>
-       							<li><p>Make sure click Save and Submit button for saving review answers</p></li>
-       								<li><p>Mal function may leads to cancel the candidate test/hold the result</p></li>
-       		</ul>
-       </div>
-       </div>
-       <div class="row">
-       		<div class="col-md-8 col-md-offset-1">
-       		
-							 <input type="checkbox" id="idcheck" name="checkbox" ><label class="last">I agree to the company's <span class="terms">Terms and Conditions</span></label>
-       	</div>
-       </div>
-  
+            <li> <span><img src="../images/wrong.jpg" class="im"></span>
+            <p class="q">Unanswered questions</p></li>
+          <li> 
+            <p class="q">Keep eye on countdown timer(look up at right top corner reverse clock)</p></li>
+            <li> 
+            <p class="q">Dont forget to click on save and exit button</p></li>
+            <li> 
+            <p class="q">Reviews answers will not be saved unless Save and Exit button is clicked</p></li>
+          </ul>
 
-<div class="row">
-		<div class="col-md-8 col-md-offset-1">
-       <div id="sub">
-      	<button class="btn btn-primary btns"  disabled="disabled">Submit</button>
+          <h3>Dos,</h3>
+          <ul>
+            <li><p>Fill all mandatory fields given in registration form without any errors</p></li>
+              <li><p>You can choose any random question in the list</p></li>
+                <li><p>You can keep review for the answer which is not sure</p></li>
+                  <li><p>You can carry only pen and paper to the test hall</p></li>
+                    <li><p>Any doubts inbetween online test,you can ask invigilator</p></li>
+          </ul>
+
+
+          <h3>Dont's</h3>
+          <ul>
+            <li><p>Should not carry any electonics gadgets</p></li>
+              <li><p>Don't use any functional keys</p></li>
+                <li><p>Do not resize the test window</p></li>
+                  <li><p>Do not refresh the page when test is conducting</p></li>
+                    <li><p>Make sure click Save and Submit button for saving review answers</p></li>
+                      <li><p>Mal function may leads to cancel the candidate test/hold the result</p></li>
+          </ul>
+          <input type="checkbox" id="idcheck" name="checkbox" ><label class="last">I agree to the company's <span class="terms">Terms and Conditions</span></label>
+          <div id="sub">
+        <button class="btn btn-primary btns"  disabled="disabled">Submit</button>
       </div>
-  	
-  </div>
-</div>
-</div>
+						</div>
+
+					</div>
+				</div>
+			</div>
+		 </div>  
+  
 </body>
 <script src="js/modules/userinstructions.js"></script> 
 </html>

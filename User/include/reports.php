d839fb6b742067ebe297aaf90a2083a92840a156
Sneha <sneha@royalsoftwaresolution.com>
2018-06-20 09:08:32 +0530

    yesteday changes on User/include/reports.php added code on to call table plugin

--- User/include/reports.php
+++ User/include/reports.php
@@ -1,13 +1,37 @@
-<link rel="stylesheet" href="css/modules/reports.css">
+<link rel="stylesheet" href="css/modules/report.css">
  <div class="row-one widgettable">
     <div class="col-md-12 content-top-2 card">
         <div class="agileinfo-cdr">
             <div class="card-header">
-                <h3>Your test result will be appear here.</h3>
-            </div>
+           
+              <div class="row">
+              		
+              	<div class="col-md-6">
+              		<div class="box1">
+              	
+              	</div>
+              	</div>
+              	<div class="col-md-6">
+              		<div class="box2">
+              		
+              	</div>
+              </div>
+              </div>
+          </div>
+      
+         
+            
+            <div class="row">
+            	<div class="col-md-12 repo">
+                 <div class="table" id="report"> </div>
+             </div>
+          </div>
+        </div>
+   
         </div>
         </div>
-    </div>      
+    </div>  
     <div class="clearfix"> </div>
-</div>  
+</div>
 <script src="js/modules/reports.js"></script>
+<script src="../js/plugin/Plugin_Table.js"></script>

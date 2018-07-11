8c4156a82c1ecec3d99fa613edfb3b91d8c3223c
Sneha <sneha@royalsoftwaresolution.com>
2018-07-11 22:32:38 +0530

    today changes on User/js/modules/reports.js added model window code n calling ajax on model window tabel pugin code

--- User/js/modules/reports.js
+++ User/js/modules/reports.js
@@ -1,39 +1,90 @@
-
 var data ;
 $(document).ready(function(){
 	$('.ng-scope').show();
+     
       $.ajax({
 		url : "../apis/GenricData.php",
 		data : {"layout" : "1010" , "userName":userName },
 		method: "POST",
 		success: function(result){
 			$('.ng-scope').hide();
-		result = JSON.parse(result);
-		data = result;
+			result = JSON.parse(result);
+			data = result;
 			$("#report").Plugin_Table({
 	            resultData: result,
 	            datatablerequired: true,
-	             })
+	            ClickColumns: "4",
+	             });
+			$("#columnchart").ChartPlugin({
+		        resultData : data,
+		        multipleYaxis: true,
+		        ChartType :"column",
+		        OutputType: 2,
+		        Refreshtime: 200000
+		    });
+
+		    $("#piechart").ChartPlugin({
+		        resultData : data,
+		        multipleYaxis: true,
+		         ChartType :"pie",
+		        OutputType: 2,
+		        Refreshtime: 200000
+		    });
 		},
 		error : function(result){
           console.log(result);
           $('.ng-scope').hide();
 		}
-	  });
 
-	 $("#barchart").ChartPlugin({
-		        resultData : data,
-		        multipleYaxis: true,
-		        OutputType: 2,
-		        Refreshtime: 200000
-		    });
-
-
+       });
 })
 
 
 
+function redirecttopage(jsonValue) { 
+$("#View").modal("show");		
+$.ajax({
+		url : "../apis/GenricData.php",
+		data : {"layout" : "1013" , "userName":userName ,"test_date" :jsonValue["Test_Date"] },
+		method: "POST",
+		success: function(result){
+	    result = JSON.parse(result);
+			data = result;
+			$("#report").Plugin_Table({ 
+	            resultData: result,
+	            datatablerequired: true,
+	            ClickColumns: "4",
+	             });
+			},
+			error : function(result){
+          console.log(result);
+          
+		}
+		
+
+       });			
+
+}
 
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	

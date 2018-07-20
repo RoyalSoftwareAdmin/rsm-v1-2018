3e8b08bf8e354a826e212a77d63f1978d932c012
Sneha <sneha@royalsoftwaresolution.com>
2018-07-20 22:33:47 +0530

    todaychanges Agent/include/addTest.php added code

--- Agent/include/addTest.php
+++ Agent/include/addTest.php
@@ -5,6 +5,7 @@
             <div class="card-header">            
 	            <div class="row">
 					<div class="col-md-12 default-margin">
+						<p class="add">Add New Test </p>
 						<div class="textareaDiv">
 		                  <label>Enter Title</label>
 		                  <input type="text" class="title"/> 
@@ -16,9 +17,10 @@
 						</div>
 
 					
-				</div>
+				</div> 
+				
 				<div class="row">
-				<div class="default-margin ">
+				<div class="default-margin year">
 					<div class="col-md-6 ">
 						<label> Year of Experience :</label>
 						
@@ -32,6 +34,19 @@
 					</div>
 					</div>
 				</div>
+
+				<div class="row">
+					<div class="col-md-12 default-margin skills">
+						
+						<div class="textareaDiv">
+		                  <label>Technical Skills</label>
+		                 <textarea > </textarea>
+						</div>
+					</div>
+				
+			
+					</div>
+				
 
 				<div class="row">
 				<div class="default-margin">
@@ -51,7 +66,7 @@
 					
 				<div class="col-md-12 btnsParent">
 					<p id="error"></p>
-				 	<button class="btn btn-success btns" onclick ="Validate()">Submit</button>
+				 	<button class="btn btn-success btns submit">Submit</button>
 				  	<button class="btn btn-danger btns">Clear</button>
 				  	
 				</div>
@@ -60,5 +75,9 @@
 	</div>
 </div>
 <div class="clearfix"> </div>
+
+
+
+
 <script type="text/javascript" src="js/common/lists.js"></script>
 <script src="js/modules/addTest.js"></script>  

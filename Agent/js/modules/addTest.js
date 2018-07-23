43a73cdb3ae03460161ac2fd94d6f68a0f21f323
Sneha <sneha@royalsoftwaresolution.com>
2018-07-23 23:21:26 +0530

    today changes on Agent/js/modules/addTest.js added ajax code

--- Agent/js/modules/addTest.js
+++ Agent/js/modules/addTest.js
@@ -1,5 +1,13 @@
 $(document).ready(function(){
-
+ 
+  var userName = JSON.parse(localStorage.getItem("session")).userName;
+  
+   /*var  entertitle = $("#entertitle").val(),
+       minimumList = $("#minimumList").val(),
+        maximumList = $("#maximumList ").val(),
+      skills = $("#skills").val(),
+        startdate = $("#startdate").val(),
+         enddate = $("#enddate").val(),*/
     // Creating dropdowns for the list items
   var minimumOption = "", maximumOption ="";
   minimumList.forEach(function(k,r){
@@ -12,14 +20,24 @@ $(document).ready(function(){
   })
   $("#maximumList").html(maximumOption);
 
+
     $(".submit").on("click", function(){
+      title = $("#entertitle").val(),
+     condition = $("#idcheck").val(),
+      min_yoe = $("#minimumList").val(),
+      max_yoe = $("#maximumList ").val(),
+      start_date= $("#startdate").val(),
+      end_date = $("#enddate").val(),
+      skills_required = $("#skills").val(),
+      
      $.ajax({
     url : "../apis/AgentData.php",
      method: "POST",
-    data : {"layout" : "3001", "userName":userName ,"title":title , "condition": condition,
+    data : {"layout" : "3001", "userName":userName ,"title":title , "condition":condition ,
     "min_yoe": min_yoe , "max_yoe": max_yoe ,"start_date" : start_date , "end_date" :end_date ,
     "skills_required" : skills_required },
      success : function(data){
+    
         console.log(data);
      
 },
@@ -28,7 +46,9 @@ $(document).ready(function(){
     }
 
        });
-   })
+   
+   
+  });
     
     $('#idcheck').change(function(e){
         if(this.checked){

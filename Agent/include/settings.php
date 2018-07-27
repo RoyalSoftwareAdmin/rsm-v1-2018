457a1377bb1b87039ede6ffcfcacf0df9132e79e
Sneha <sneha@royalsoftwaresolution.com>
2018-07-27 10:53:09 +0530

    added new  settings.php file   in Agent folder

---
+++ Agent/include/settings.php
@@ -0,0 +1,42 @@
+ <script type="text/javascript" src="js/common/common.js"></script>
+ <link rel="stylesheet" href="css/modules/settings.css">
+ <div class="row-one widgettable">
+    <div class="col-md-12 content-top-2 card">
+        <div class="agileinfo-cdr">
+            <div class="card-header">
+                
+                <div class="container-fluid">
+                <div class="row">
+                	<div class = "col-md-6">
+                		<label> Enter New Password</label>
+                		<input type="password" name="password" id="password" placeholder="Enter New Password" >
+                        
+                	</div>
+                	
+                	<div class = "col-md-6">
+                		<label>  Confirm Password</label>
+                		<input type="password" name="repassword" id="repassword" placeholder="Enter Confirm Password" >
+                	</div>
+
+                </div>
+
+                
+                	<div class="container-fluid">
+                		<div class="sub_home">
+                			<div class="row">
+                				<div class="col-md-12">
+								<input type="button" id="submit" value="Submit">
+							
+						</div>
+					</div>
+                       <div class="error"></div>
+                </div>
+                </div>
+            </div>
+            </div>
+        </div>
+        </div>
+    </div>      
+    <div class="clearfix"> </div>
+</div> 
+<script src="js/modules/settings.js"></script>   
